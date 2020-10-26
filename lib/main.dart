b8a1c04bb6c770afe34f4dfcb099a0c7b82e1988
Kijacode <kijadanford@gmail.com>
2020-10-26 23:56:51 +0300

    refactor: remove stetho package on CI/CD build fail

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:onsite/app.dart';
-import 'package:flutter_stetho/flutter_stetho.dart';
 
 
 void main() {
-   Stetho.initialize();
   runApp(MyApp());
 }
 
